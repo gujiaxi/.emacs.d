29e327c516776b882ee5060312125755793e3f72
Jiaxi Gu <imjiaxi@gmail.com>
2019-07-17 11:39:44 +0800

    remove org-drill support because it has been removed from org-mode core repo

--- init.el
+++ init.el
@@ -340,8 +340,6 @@ The site configuration is defined in index.org."
           ("frame" "single")))
   ;; ox-bibtex
   (require 'ox-bibtex)
-  ;; org-drill
-  (require 'org-drill)
   ;; org-babel
   (org-babel-do-load-languages
    'org-babel-load-languages
