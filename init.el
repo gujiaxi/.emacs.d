a6cf9cc2a3a13903067d5b7eb857248825eb3fe9
Jiaxi Gu <imjiaxi@gmail.com>
2020-03-23 22:34:48 +0800

    simplify file coding settings

--- init.el
+++ init.el
@@ -37,10 +37,7 @@
 (setq user-mail-address "imjiaxi@gmail.com")
 
 ;; set unicode encoding
-(prefer-coding-system 'utf-8-unix)
-(set-default-coding-systems 'utf-8-unix)
-(set-terminal-coding-system 'utf-8-unix)
-(set-keyboard-coding-system 'utf-8-unix)
+(prefer-coding-system 'utf-8)
 
 ;; no lockfile
 (setq create-lockfiles nil)
